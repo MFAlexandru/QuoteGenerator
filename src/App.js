5ec30a51e88a30e130bdaf86a7f6506bd01c2bae
Margarit Alexandru <margaritalexandru@pop-os.localdomain>
2021-07-01 13:03:57 +0300

    Made page skelet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import "./App.css";
+
+import { connect } from "react-redux"
+
+import {
+  increaseCounter,
+  decreaseCounter
+} from "./redux/Counter/counter.actions"
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id = "quote-box" className = "App">
+      <p id = "text">Some quote!</p>
+      <p id = "author">The Author!</p>
+      <button onClick = {() => props.increaseCounter()}>Next Quote</button>
+      <a id = "tweet-quote" target = "_blank" href = "https://twitter.com/intent/tweet">Tweet it</a>
     </div>
   );
-}
+};
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    count: state.counter.count,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increaseCounter: () => dispatch(increaseCounter()),
+    decreaseCounter: () => dispatch(decreaseCounter()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
